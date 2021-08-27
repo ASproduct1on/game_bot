9aa1ad25b7f5790f80e90a7060ec04bf3ab4907f
Andrii Skega <Game.a.s.production@gmail.com>
2021-08-27 13:57:27 +0200

    gameBot work

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
-"use strict"
+"use strict";
+
+
+// function gameBot (number) {
+//     let resault = false;
+
+//     if (!resault) {
+//         const answer = +prompt('Угадай число от 1 до 100?');
+//         if (answer) {
+//             number === answer;
+//             console.log("Поздравляю, Вы угадали!!!");
+//         }
+//         else if {
+
+//         }
+//     } 
+    
+// }
+
+function gameBot (number) {
+    const answer = prompt('Угадай число от 1 до 100?');
+
+    if(isNaN(parseFloat(answer))){
+        console.log("Введи число!");
+        gameBot(number);
+    }else if(+answer === number){
+        alert('Поздравляю, Вы угадали!!!');
+    }else if (+answer > number) {
+        console.log("Загаданное число меньше");
+        gameBot(number);
+    } else if (+answer < number) {
+        console.log("Загаданное число больше");
+        gameBot(number);
+    } else if (+answer === false) {
+        console.log ("Игра окончена")
+    } else {
+        gameBot(number);
+    }    
+}
+
+gameBot(22);
+
+
 
