4dd726a9337037a7e4717a8f1aebfd08f2ce7156
Andrii Skega <Game.a.s.production@gmail.com>
2021-09-08 10:27:04 +0200

    add alert

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function gameBot (number) {
     const answer = prompt('Угадай число от 1 до 100?');
 
     if(isNaN(parseFloat(answer))){
-        console.log("Введи число!");
+        alert("Введи число!");
         gameBot(number);
     }else if(+answer === number){
         alert('Поздравляю, Вы угадали!!!');
     }else if (+answer > number) {
-        console.log("Загаданное число меньше");
+        alert("Загаданное число меньше");
         gameBot(number);
     } else if (+answer < number) {
-        console.log("Загаданное число больше");
+        alert("Загаданное число больше");
         gameBot(number);
     } else if (+answer === false) {
-        console.log ("Игра окончена")
+        alert("Игра окончена");
     } else {
         gameBot(number);
     }    
