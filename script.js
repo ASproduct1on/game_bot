436b4256938d36bb132c690476465630b91069ba
ASproduct1on <88718694+ASproduct1on@users.noreply.github.com>
2021-09-08 14:40:49 +0200

    full bot

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-"use strict"
+"use strict";
+
+function gameBot (number) {
+    const answer = prompt('Угадай число от 1 до 100?');
+
+    if(isNaN(parseFloat(answer))){
+        alert("Введи число!");
+        gameBot(number);
+    }else if(+answer === number){
+        alert('Поздравляю, Вы угадали!!!');
+    }else if (+answer > number) {
+        alert("Загаданное число меньше");
+        gameBot(number);
+    } else if (+answer < number) {
+        alert("Загаданное число больше");
+        gameBot(number);
+    } else if (+answer === false) {
+        alert("Игра окончена");
+    } else {
+        gameBot(number);
+    }    
+}
+
+gameBot(22);
+
+
 
